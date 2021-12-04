1e1004907e2258be38caaad4493e0e7a0d4dcb38
sebastiangarciaacosta <sebastian.garcia@wearemo.com>
2021-12-03 21:46:07 -0500

    Generated second better video

--- plot_results.py
+++ plot_results.py
@@ -2,12 +2,14 @@ from dataclasses import dataclass
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from video_processing import read_video_array
-from typing import Dict, Tuple, Union, List
+from typing import Any, Dict, Tuple, Union, List
+from dataclasses import field
 import pandas as pd
 import numpy as np
 
 START_FRAME = 142
 END_FRAME = 268
+FIG_SIZE = (20, 10)
 
 graphs = {
     (
@@ -51,7 +53,7 @@ def make_figures() -> Tuple[plt.Figure, plt.Axes, Result]:
     widths = [4, 4, 4]
     heights = [1, 1, 1]
 
-    fig = plt.figure()
+    fig = plt.figure(figsize=FIG_SIZE)
     gs = fig.add_gridspec(
         ncols=3,
         nrows=3,
@@ -85,49 +87,104 @@ class DataGenerator(object):
     def __post_init__(self):
         self.current_frame = self.start_frame
 
-    def __call__(self) -> Tuple[np.ndarray, Result, Result]:
+    def __call__(self) -> Tuple[float, np.ndarray, Result, Result]:
         while self.current_frame < self.end_frame:
             frame = self.video[self.current_frame]
             exp = self.get_result(self.expdf)
             theo = self.get_result(self.theodf)
+            time = self.expdf.loc[self.current_frame, "t"]
             self.current_frame += 1
-            return frame, exp, theo
+            yield time, frame, exp, theo
 
     def get_result(self, df: pd.DataFrame) -> Result:
         result_args: Dict[str, float] = dict()
 
         for col in graphs.values():
-            print(df)
-            print(self.current_frame)
             result_args[col] = df.loc[self.current_frame, col]
         return Result(**result_args)
 
 
 @dataclass
 class AnimationRunner(object):
-    video_axes: plt.Axes
-    values_axes: Result
+    subfigs: Dict[str, plt.Axes]
 
     def __post_init__(self):
         self.time: List[float] = []
+        self.exp_data: Dict[str, List[float]] = dict()
+        self.theo_data: Dict[str, List[float]] = dict()
 
-    def __call__(self, data: Tuple[np.ndarray, Result, Result]):
-        frame, exp, theo = data
-        self.video_axes.set_array(frame)
-        return self.video_axes
+        for sysmetric in graphs.values():
+            self.exp_data[sysmetric] = []
+            self.theo_data[sysmetric] = []
+
+    def __call__(self, data: Tuple[float, np.ndarray, Result, Result]):
+        time, frame, exp, theo = data
+        self.time.append(time)
+        for axis in ["x", "y"]:
+            self.subfigs["video"].tick_params(
+                axis=axis,  # changes apply to the x-axis
+                which="both",  # both major and minor ticks are affected
+                bottom=False,  # ticks along the bottom edge are off
+                top=False,  # ticks along the top edge are off
+                labelbottom=False,
+            )  # labels along the bottom edge are off
+
+        self.subfigs["video"].imshow(frame)
+        for sysmetric in graphs.values():
+            self.exp_data[sysmetric].append(getattr(exp, sysmetric))
+            self.theo_data[sysmetric].append(getattr(theo, sysmetric))
+            sysfig: plt.Axes = self.subfigs[sysmetric]
+            sysfig.clear()
+            sysfig.grid()
+            sysfig.plot(
+                self.time,
+                self.exp_data[sysmetric],
+                label="experimental",
+                color="red",
+                linestyle="dashed",
+                marker="o",
+                markersize=5,
+            )
+            sysfig.plot(
+                self.time,
+                self.theo_data[sysmetric],
+                label="theoretical",
+                color="blue",
+                linestyle="dashed",
+                marker="o",
+                markersize=5,
+            )
+            sysfig.set_title(sysmetric)
+            sysfig.legend()
+        return self.subfigs.values()
+
+
+def initialize_animation(
+    video_axes: plt.Axes, values_axes: Result, h: int, w: int
+) -> Dict[str, plt.Axes]:
+    video_axes.set_title("Video")
+    init_image = np.zeros((h, w, 3), dtype=np.uint8)
+    video_axes.imshow(init_image)
+    axes = {"video": video_axes}
+
+    for sysmetric in graphs.values():
+        ax = getattr(values_axes, sysmetric)
+        ax.plot([], [])
+        ax.set_title(sysmetric)
+        axes[sysmetric] = ax
+    return axes
 
 
 def main():
     video_arr, fps = read_video_array("data/raw/fisica_video.mp4")
     exp = pd.read_csv("data/processed/experimental_system.csv", index_col="frame")
     tho = pd.read_csv("data/processed/theoretical_system.csv", index_col="frame")
-
-    print(video_arr.shape)
+    time_btw_frames = (exp["t"].values[1:] - exp["t"].values[:-1]).mean()
 
     fig, video_axes, axes = make_figures()
-    video_axes.set_title("Video")
-    init_img = np.random.rand(video_arr.shape[-2], video_arr.shape[-1])
-    video_axes.imshow(init_img)
+    subfigs = initialize_animation(
+        video_axes, axes, video_arr.shape[1], video_arr.shape[2]
+    )
 
     data_gen = DataGenerator(
         video=video_arr,
@@ -136,13 +193,18 @@ def main():
         expdf=exp,
         theodf=tho,
     )
-    runner = AnimationRunner(video_axes=video_axes, values_axes=axes)
-
+    runner = AnimationRunner(
+        subfigs=subfigs,
+    )
+    plt.title("Simple pendulum")
+    fig.suptitle("Experimental vs theoretical results")
     ani = animation.FuncAnimation(
-        fig, runner, data_gen, blit=True, interval=60, repeat=False
+        fig, runner, data_gen, repeat=False, blit=False, interval=time_btw_frames
     )
     plt.tight_layout()
-    plt.show()
+    ffwriter = animation.FFMpegWriter(fps=fps)
+    ani.save("results_2.mp4", writer=ffwriter)
+    # plt.show()
 
 
 if __name__ == "__main__":
